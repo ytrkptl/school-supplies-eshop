6d488e514420f9a947c34aea10d970e3bbfa1e11
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2025-01-27 20:42:03 -0500

    Modified vite config to support styled-components displayname by adding support for babel.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,22 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: false,
+              pure: true
+            }
+          ]
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
