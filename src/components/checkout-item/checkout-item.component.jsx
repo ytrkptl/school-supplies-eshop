0a0dcd7c98d17ad260e9429831b27e57c93d812f
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2025-01-27 20:44:22 -0500

    Made checkout form more responsive.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -7,35 +7,79 @@ import {
   removeItem
 } from '@/redux/cart/cart.reducer';
 
+import useWindowSize from '../../hooks/useWindowSize';
+
 import {
   CheckoutItemContainer,
   ImageContainer,
   TextContainer,
+  QuantityControlsContainer,
   QuantityContainer,
-  RemoveButtonContainer
+  RemoveButtonContainer,
+  MobileRowContainer
 } from './checkout-item.styles';
 
 const CheckoutItem = ({ cartItem }) => {
   const dispatch = useDispatch();
   const { name, imageUrl, price, quantity } = cartItem;
+  const { width } = useWindowSize();
+  const isMobile = width <= 600;
 
   const handleClearItem = () => dispatch(clearItemFromCart(cartItem));
   const handleAddItem = () => dispatch(addItem(cartItem));
   const handleRemoveItem = () => dispatch(removeItem(cartItem));
+
+  const QuantityControls = () => (
+    <QuantityContainer>
+      <div onClick={handleRemoveItem}>&#10094;</div>
+      <span>{quantity}</span>
+      <div onClick={handleAddItem}>&#10095;</div>
+    </QuantityContainer>
+  );
+
+  const RemoveButton = ({ className }) => (
+    <RemoveButtonContainer onClick={handleClearItem} className={className}>
+      &#10005;
+    </RemoveButtonContainer>
+  );
+
+  if (isMobile) {
+    return (
+      <CheckoutItemContainer>
+        <MobileRowContainer>
+          <ImageContainer>
+            <img src={imageUrl} alt='item' />
+          </ImageContainer>
+          <QuantityControlsContainer>
+            <QuantityControls />
+            <RemoveButton />
+          </QuantityControlsContainer>
+        </MobileRowContainer>
+        <TextContainer data-label="Description">
+          {name}
+        </TextContainer>
+        <TextContainer data-label="Price">
+          ${price}
+        </TextContainer>
+      </CheckoutItemContainer>
+    );
+  }
 
   return (
     <CheckoutItemContainer>
       <ImageContainer>
         <img src={imageUrl} alt='item' />
       </ImageContainer>
-      <TextContainer>{name}</TextContainer>
-      <QuantityContainer>
-        <div onClick={handleRemoveItem}>&#10094;</div>
-        <span>{quantity}</span>
-        <div onClick={handleAddItem}>&#10095;</div>
-      </QuantityContainer>
-      <TextContainer>${price}</TextContainer>
-      <RemoveButtonContainer onClick={handleClearItem}>
+      <TextContainer data-label="Description">
+        {name}
+      </TextContainer>
+      <QuantityControlsContainer data-label="Quantity">
+        <QuantityControls />
+      </QuantityControlsContainer>
+      <TextContainer data-label="Price">
+        ${price}
+      </TextContainer>
+      <RemoveButtonContainer onClick={handleClearItem} className="desktop-only">
         &#10005;
       </RemoveButtonContainer>
     </CheckoutItemContainer>
--- src/components/checkout-item/checkout-item.styles.jsx
+++ src/components/checkout-item/checkout-item.styles.jsx
@@ -1,44 +1,103 @@
 import styled from 'styled-components';
 
-export const CheckoutItemContainer = styled.div`
-  width: 100%;
-  display: flex;
-  min-height: 100px;
-  min-width: 300px;
-  border-bottom: 1px solid darkgrey;
-  padding: 15px 0;
-  font-size: 20px;
-  align-items: center;
-  
-  @media screen and (max-width: 600px) {
-    padding: 0;
+export const CheckoutItemContainer = styled.tr`
+  td {
+    padding: 15px 10px;
+
+    @media screen and (max-width: 600px) {
+      width: 100%;
+      padding: 10px 0;
+      font-size: 16px;
+    }
   }
 `;
 
-export const ImageContainer = styled.div`
+export const MobileRowContainer = styled.td`
+  @media screen and (max-width: 600px) {
+    display: flex !important;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding: 10px !important;
+
+    &:before {
+      display: none;
+    }
+  }
+`;
+
+export const ImageContainer = styled.td`
   width: 23%;
-  padding-right: 15px;
+  padding: 15px;
+
   img {
     width: 100%;
-    height: 100%;
+    max-width: 150px;
+    height: auto;
+    display: block;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: auto;
+    padding: 0;
+    
+    img {
+      max-width: 80px;
+      margin: 0;
+    }
   }
 `;
 
-export const TextContainer = styled.span`
+export const TextContainer = styled.td`
   width: 23%;
 `;
 
-export const QuantityContainer = styled(TextContainer)`
+export const QuantityControlsContainer = styled.td`
+  width: 23%;
+
+  @media screen and (max-width: 600px) {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 0.2em;
+
+    td {
+      justify-content: center !important;
+    }
+  }
+`;
+
+export const QuantityContainer = styled.div`
   display: flex;
+  align-items: center;
+  
+  div {
+    cursor: pointer;
+    user-select: none;
+    padding: 0 10px;
+  }
+
   span {
     margin: 0 10px;
   }
-  div {
-    cursor: pointer;
+
+  @media screen and (max-width: 600px) {
+    margin-left: 1em;
+    
+    div {
+      padding: 5px 10px;
+    }
   }
 `;
 
-export const RemoveButtonContainer = styled.div`
+export const RemoveButtonContainer = styled.td`
   padding-left: 12px;
   cursor: pointer;
+  text-align: center;
+
+  @media screen and (max-width: 600px) {
+    padding: 5px 10px;
+    font-size: 18px;
+    left
+  }
 `;
