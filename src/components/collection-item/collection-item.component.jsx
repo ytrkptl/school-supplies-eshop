afbc07a8880549cd69ea50ec9fd85cab63032327
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2025-02-06 21:06:46 -0500

    Fixed import statement error.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useBagOperations } from '../bag-operations/bag-operations.component';
-
+import useBagOperations from '../bag-operations/bag-operations.component';
 import {
   CollectionItemContainer,
   CollectionFooterContainer,
